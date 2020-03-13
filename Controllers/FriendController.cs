8911d5fbdd22cb1c673f5ee493c0fd5a2288dd58
jacheva <61788106+jacheva@users.noreply.github.com>
2020-03-13 12:20:58 +0100

    First commit

---
+++ Controllers/FriendController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BootsrapDemoTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootsrapDemoTest.Controllers
+{
+    public class FriendController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private static List<FriendModel> friends = new List<FriendModel>()
+        {
+            new FriendModel()
+            {
+                FriendID = 0,
+                Name = "Sandra Jacheva",
+                Hometown = "Veles"
+            }
+        };
+
+        // GET: Friend
+        public ActionResult AllFriends()
+        {
+            return View(friends);
+        }
+
+        public ActionResult AddFriend()
+        {
+            FriendModel model = new FriendModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddFriend(FriendModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("AddFriend", model);
+            friends.Add(model);
+            return RedirectToAction("AllFriends", "Friend");
+        }
+
+        public ActionResult EditFriend(int id)
+        {
+            var model = friends.ElementAt(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditFriend(FriendModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("EditFriend", model);
+            friends.RemoveAt(model.ID);
+            friends.Insert(model.ID, model);
+            return RedirectToAction("AllFriends", "Friend");
+        }
+
+        public ActionResult DeleteFriend(int id)
+        {
+            friends.RemoveAt(id);
+            return RedirectToAction("AllFriends", "Friend");
+        }
+    }
+}
---
+++ Models/FriendModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootsrapDemoTest.Models
+{
+    public class FriendModel
+    {
+        public int ID { get; set; }
+        [Required]
+        [Range(0, 200, ErrorMessage = "FriendID must be between 0-200")]
+        [Display(Name = "Friend ID")]
+
+        public int FriendID { get; set; }
+        [Required] public String Name { get; set; }
+        [Required] public String Hometown { get; set; }
+
+
+
+
+    }
+}
